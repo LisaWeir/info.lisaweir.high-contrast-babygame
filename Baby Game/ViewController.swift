cfe48983bdfc88b327cabf07fd6e7b9aaf60cc63
James Weir <jamesmweir3@gmail.com>
2015-02-11 15:20:22 -0800

    Resized the background images to correct resolutions

--- Baby Game/ViewController.swift
+++ Baby Game/ViewController.swift
@@ -14,11 +14,25 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        var alert = UIAlertController(title: "Alert", message: "Ran out of gas.", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "K", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
 
-        fadeOut()
+       // fadeImageOut()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        var alert = UIAlertController(title: "Alert", message: "Ran out of gas.", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "K", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
     }
     
-    func fadeOut() {
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+
+    func fadeImageOut() {
             UIView.animateWithDuration(0.5, delay: 5, options: nil, animations: {
                     self.mainImage.alpha = 0
                 }, completion: { finished in
@@ -26,11 +40,11 @@ class ViewController: UIViewController {
                 })
     }
     
-    func fadeIn() {
+    func fadeImageIn() {
         UIView.animateWithDuration(0.5, delay: 0, options: nil, animations: {
                 self.mainImage.alpha = 1
             }, completion: { finished in
-                self.fadeOut()
+                self.fadeImageOut()
             })
     }
     
@@ -38,24 +52,14 @@ class ViewController: UIViewController {
         let imageNames = ["stars", "circles", "chevron", "squares", "checkers"]
         weak var selectedImage = mainImage.image
         
-        while(self.mainImage.image == selectedImage) {
+        while(mainImage.image == selectedImage) {
             let randomNumber = arc4random_uniform(UInt32(imageNames.count))
             selectedImage = UIImage(named: imageNames[Int(randomNumber)])!
         }
-        self.mainImage.image = selectedImage
+        mainImage.image = selectedImage
         selectedImage = nil
-        self.fadeIn()
+        fadeImageIn()
     }
-
-    
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-    
-    override func prefersStatusBarHidden() -> Bool {
-        return true
-    }
-    
+        
 }
 
